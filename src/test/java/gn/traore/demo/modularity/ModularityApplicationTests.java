7a947ac3fa7584232c17851b93b3c0a9b89eb416
Ibrahima <ibrahima982011@koonda.fr>
2019-12-26 13:50:42 +0000

    Initialisation du projet.

---
+++ src/test/java/gn/traore/demo/modularity/ModularityApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.traore.demo.modularity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModularityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
